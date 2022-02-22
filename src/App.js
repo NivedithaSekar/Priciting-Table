453bc39a583b092dba3b74ce6005b8cd65d7a84b
NivedithaSekar <sekarniveditha@gmail.com>
2022-02-22 22:46:49 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = require('./products.json').products;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="pricing">
+        <div className="container">
+          <div className="row">
+            {products.map((product) => {
+              return <Product details={product}/>
+            })}
+          </div>
+        </div>
+      </section>
     </div>
+  );
+}
+
+function Product(props){
+  let template = "";
+  return(
+      <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+          <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">{props.details.subscription}</h5>
+            <h6 className="card-price text-center">{props.details.price}<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="fa-ul">
+              {
+                (Object.entries(props.details.features)).map((feature)=>{
+                    if(feature[1]){
+                      return <li><span className="fa-li"><i className="fa fa-check"></i></span>{(typeof(feature[1]) === "boolean") ? feature[0] : feature[1]}</li>;
+                    }else{
+                      return <li className="text-muted"><span className="fa-li"><i className="fa fa-times"></i></span>{feature[0]}</li>;
+                    }
+                  })
+              }
+            </ul>
+            <div className="d-grid">
+              <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
